7e9500648830de36fb7b600901396ee41ad104f5
Markus <m.hoffmann65@gmx.de>
2025-03-03 10:13:32 +0100

    bootstrap initialisiert

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7uht7sd9vkg"
+    "revision": "0.pnita1k9aig"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
