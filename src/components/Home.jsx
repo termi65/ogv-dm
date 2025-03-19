116d8a56b9f1c6875a44416cd096f02fc5000cb9
Markus <m.hoffmann65@gmx.de>
2025-03-19 16:38:41 +0100

    Im Startbildschirm den Abstand oben erhöht.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,9 +18,9 @@ const Home = () => {
         return () => authListener?.subscription.unsubscribe();
     }, []);
     return (
-        <div className="d-flex flex-column  align-items-center">
+        <div className="d-flex flex-column  align-items-center mt-3">
             <h1><i className="bi bi-file-person"></i> <span className="text-decoration-underline">OGV-DM</span> <i className="bi bi-pencil-square"></i></h1>
-            <h2>Willkommen zum OGV-DM (Deckelmanager vom OGV)</h2>
+            <h2 className="text-center">Willkommen zum OGV-DM (Deckelmanager vom OGV)</h2>
             <div><p className="text-decoration-underline">{user ? `angemeldet als ${user.identities[0].email}` : 'Gast'}</p></div>
             <section>Mit dieser App kannst du schnell neue Mitglieder und Getränke anlegen und den Deckel jedes Mitglieds einsehen.</section>
             <h3>ACHTUNG</h3>
